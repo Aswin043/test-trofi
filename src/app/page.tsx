3cc4a175175f105e04fa3ceb9c43d786357a7ced
Aswin043 <ashwiin04@gmail.com>
2025-08-30 19:41:24 +1000

    almost done lol acc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -275,7 +275,7 @@ export default function Home() {
       }
 
       const script = document.createElement('script');
-      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBxN29TEo8SEzFGqwR2LAp3XvlquPAsCO4&libraries=places,geometry`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places,geometry`;
       script.async = true;
       script.defer = true;
       script.onload = initializeMap;
